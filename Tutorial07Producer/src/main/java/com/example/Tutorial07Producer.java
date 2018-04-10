aff60b4cea27496c5f676af2bd8fd1016db9124e
KurniawanHendiW <kurniawanhendiwijaya@gmail.com>
2018-04-10 20:31:29 +0700

    first

---
+++ Tutorial07Producer/src/main/java/com/example/Tutorial07Producer.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tutorial07Producer
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (Tutorial07Producer.class, args);
+    }
+}
